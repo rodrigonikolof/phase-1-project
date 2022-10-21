d67f9cf120d0d80303b2b218f19de00d8fb46ae1
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2022-10-22 08:30:46 +1100

    Positioned dark mode toggle at the top right, deleted one of the buttons on the top bar

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-console.log('Hello world again!')
+// fetch('https://v2.jokeapi.dev/joke/Any?safe-mode')  Joke API
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+	
+    // fetch('https://api.thecatapi.com/v1/images/search')    cat API
+	// .then(response => response.json())
+	// .then(response => console.log(response))
 
+	
+    // https://random.dog/woof.json   dog API
+
+
+    // https://dog.ceo/dog-api/documentation/random another dog API
+
+    // https://cataas.com/#/       cat API
+
+    
+    // random useless fact 
+    // https://uselessfacts.jsph.pl/random
+// function getAPI (){ 
+//     fetch('https://random.dog/woof.json')    
+// 	.then(response => response.json())
+// 	.then(response => console.log(response));}
+
+//     setTimeout(getAPI,2000)
