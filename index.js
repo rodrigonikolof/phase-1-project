712df1fb91ec8eb2cff4f487fbfdc353b682d207
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2022-10-22 16:07:40 +1100

    Commit 7: Implemented fetch for dog and cat API and rendered them on main div

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-// fetch('https://v2.jokeapi.dev/joke/Any?safe-mode')  Joke API
-// 	.then(response => response.json())
-// 	.then(response => console.log(response))
-	
     // fetch('https://api.thecatapi.com/v1/images/search')    cat API
 	// .then(response => response.json())
 	// .then(response => console.log(response))
 
-	
     // https://random.dog/woof.json   dog API
 
 
@@ -14,12 +9,44 @@
 
     // https://cataas.com/#/       cat API
 
-    
-    // random useless fact 
-    // https://uselessfacts.jsph.pl/random
-// function getAPI (){ 
-//     fetch('https://random.dog/woof.json')    
-// 	.then(response => response.json())
-// 	.then(response => console.log(response));}
 
-//     setTimeout(getAPI,2000)
+function getCats(){
+    fetch('https://api.thecatapi.com/v1/images/search')
+    .then(response => response.json())
+    .then (data => renderCatPicture(data[0].url)) 
+}
+function getDogs (){ 
+    fetch('https://random.dog/woof.json')    
+	.then(response => response.json())
+	.then(data => renderDogPicture(data.url))
+}
+
+document.querySelector('#catBtn').addEventListener('click', getCats);
+document.querySelector('#dogBtn').addEventListener('click', getDogs);
+const main = document.querySelector('.main');
+let imageUrl;
+
+function clearMain(){
+    main.innerHTML = "";
+}
+
+function renderCatPicture(data){
+    clearMain();
+    main.innerHTML = `<img class='mainPic'src='${data}'>
+    <button id='bookmarkBtn'>Save For Later</button>
+    <button id='nextCat'>Show Me More Cats</button>`
+    document.querySelector('#nextCat').addEventListener('click', getCats)
+    imageUrl = data;
+    document.querySelector('#bookmarkBtn').addEventListener('click', ()  => console.log(imageUrl))
+}
+
+function renderDogPicture(data){
+    clearMain();
+    main.innerHTML = `<img class='mainPic'src='${data}'>
+    <button id='bookmarkBtn'>Save For Later</button>
+    <button id='nextDog'>Show Me More Dogs</button>`
+    document.querySelector('#nextDog').addEventListener('click', getDogs)
+    imageUrl = data;
+    document.querySelector('#bookmarkBtn').addEventListener('click', ()  => console.log(imageUrl))
+}
+
